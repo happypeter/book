bb5e6fc2047930f9743b5c8ddaf9c4af04eee05d
Peter Wang <happypeter1983@gmail.com>
2018-02-11 22:46:48 +0800

    async #10

--- gatsby/createPages.js
+++ gatsby/createPages.js
@@ -1,35 +1,31 @@
 const path = require(`path`)
 
-module.exports = ({ graphql, boundActionCreators }) => {
+module.exports = async ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators
-  return new Promise((resolve, reject) => {
-    graphql(`
-      {
-        allMarkdownRemark {
-          edges {
-            node {
-              fields {
-                slug
-              }
+  const allMarkdown = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
             }
           }
         }
       }
-    `).then(result => {
-      const { edges: posts } = result.data.allMarkdownRemark
-      posts.map(({ node }) => {
-        const { slug } = node.fields
-        const pid = Number(slug.split('/')[2])
-        createPage({
-          path: slug,
-          component: path.resolve(`./src/templates/book-post.js`),
-          context: {
-            slug,
-            pid
-          }
-        })
-      })
-      resolve()
+    }
+  `)
+
+  allMarkdown.data.allMarkdownRemark.edges.map(({ node }) => {
+    const { slug } = node.fields
+    const pid = Number(slug.split('/')[2])
+    createPage({
+      path: slug,
+      component: path.resolve(`./src/templates/book-post.js`),
+      context: {
+        slug,
+        pid
+      }
     })
   })
 }
