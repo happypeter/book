a05001d3255f6f60560742ec3a248b889251ed20
Peter Wang <happypeter1983@gmail.com>
2018-02-11 23:26:08 +0800

    refactor node script

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,3 @@
-const { createFilePath } = require(`gatsby-source-filesystem`)
-
-exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
-  const { createNodeField } = boundActionCreators
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug
-    })
-  }
-}
+exports.onCreateNode = require('./gatsby/onCreateNode')
 
 exports.createPages = require('./gatsby/createPages')
---
+++ gatsby/onCreateNode.js
@@ -0,0 +1,13 @@
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+module.exports = ({ node, getNode, boundActionCreators }) => {
+  const { createNodeField } = boundActionCreators
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug
+    })
+  }
+}
