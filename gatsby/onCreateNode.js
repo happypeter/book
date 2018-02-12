9a243289d0652ad77e300ce1ca8a8fbd96ebfc76
Peter Wang <happypeter1983@gmail.com>
2018-02-12 09:51:29 +0800

    add bookId to episode node #10

--- gatsby/onCreateNode.js
+++ gatsby/onCreateNode.js
@@ -2,6 +2,18 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 
 module.exports = ({ node, getNode, boundActionCreators }) => {
   const { createNodeField } = boundActionCreators
+
+  if (node.internal.type === `TocJson`) {
+    const fullPath = createFilePath({ node, getNode })
+
+    const bookId = fullPath.split('/')[1]
+
+    createNodeField({
+      node,
+      name: `bookId`,
+      value: bookId
+    })
+  }
   if (node.internal.type === `MarkdownRemark`) {
     const slug = createFilePath({ node, getNode, basePath: `pages` })
     createNodeField({
