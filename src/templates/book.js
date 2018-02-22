c5e1ee23e77c7b95d628e154e29cb18669dbfbb2
Peter Wang <happypeter1983@gmail.com>
2018-02-22 11:48:34 +0800

    title and img #21

--- src/templates/book.js
+++ src/templates/book.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 import Link from 'gatsby-link'
+import { coverPngUrl } from '../constants/Assets'
+import books from '../../config/index.json'
 import Header from '../components/Header'
 
 export default ({ data, pathContext }) => {
   const { bookId } = pathContext
-
+  const book = books.find(t => t.id === bookId)
   const toc = data.allTocJson.edges.map(t => t.node.episodeId)
   return (
     <Wrap>
       <Header bookId={bookId} />
+      <Img src={coverPngUrl(book)} />
+      <Title>{book.title}</Title>
       {toc.map(t => (
         <StyledLink key={t} to={`${bookId}/${t}`}>
           {t}
@@ -41,3 +45,6 @@ const StyledLink = styled(Link)`
   padding: 10px;
   margin: 5px;
 `
+
+const Img = styled.img``
+const Title = styled.div``
