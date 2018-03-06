a6d41dd6c2da87eadef83f0b62031e655140abbc
Peter Wang <happypeter1983@gmail.com>
2018-03-06 10:14:47 +0800

    docSearch #6

---
+++ src/components/DocSearch.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+class DocSearch extends Component {
+  state = {
+    enabled: true
+  }
+
+  componentDidMount() {
+    if (window.docsearch) {
+      window.docsearch({
+        apiKey: '731d11ffae6b19a7479686c043b7e099',
+        indexName: 'haoqicat',
+        inputSelector: '#algolia-doc-search'
+      })
+    } else {
+      console.warn('Search has failed to load and now is being disabled')
+      this.setState({ enabled: false })
+    }
+  }
+
+  render() {
+    const { enabled } = this.state
+
+    return enabled ? (
+      <Search>
+        <img src={require('../img/Shape.svg')} />
+        <input id="algolia-doc-search" type="search" />
+      </Search>
+    ) : null
+  }
+}
+
+export default DocSearch
+
+const Search = styled.div`
+  position: relative;
+  img {
+    position: absolute;
+    top: 15px;
+    left: 15px;
+    z-index: 1000;
+  }
+  input {
+    width: 325px;
+    height: 54px;
+    background-color: #a7f0f8;
+    border: none;
+    border-radius: 5px;
+    font-size: 24px;
+    color: #151313;
+    padding-left: 50px;
+  }
+  input:focus {
+    outline: none;
+  }
+`
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import Link from 'gatsby-link'
 import books from '../../config/index.json'
+import Search from '../components/DocSearch'
 
 export default ({ bookId }) => {
   const book = books && books.find(t => t.id === bookId)
@@ -18,10 +19,7 @@ export default ({ bookId }) => {
           <StyledLink to={`/${book.id}`}>&nbsp;> {book.title}</StyledLink>
         )}
       </Nav>
-      <Search>
-        <img src={require('../img/Shape.svg')} />
-        <input type="text" />
-      </Search>
+      <Search />
     </Header>
   )
 }
@@ -47,26 +45,4 @@ const StyledLink = styled(Link)`
 `
 const Nav = styled.div`
   display: flex;
-`
-
-const Search = styled.div`
-  position: relative;
-  img {
-    position: absolute;
-    top: 15px;
-    left: 15px;
-  }
-  input {
-    width: 325px;
-    height: 54px;
-    background-color: #a7f0f8;
-    border: none;
-    border-radius: 5px;
-    font-size: 24px;
-    color: #151313;
-    padding-left: 50px;
-  }
-  input:focus {
-    outline: none;
-  }
 `
